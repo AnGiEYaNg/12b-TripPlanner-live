53469f2b284186918695a3c5bd603e2f0746dca3
Angie <angi3yang@gmail.com>
2015-06-07 17:05:36 -0400

    Complete:

--- app.js
+++ app.js
@@ -1,53 +1,30 @@
-var express = require("express");
-var swig = require("swig");
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
-var jquery = require('jquery');
+var express = require('express')
+var swig = require('swig')
+var nodeSass = require('node-sass-middleware')
+var logger = require('morgan')
 
-var routes = require('./routes/index');
+var app = express()
+var path = require('path')
+module.exports = app
 
-var app = express();
+app.use(logger('dev'))
 
+var sassMiddleware = nodeSass({
+  src: __dirname + '/assets',
+  dest: __dirname + '/public',
+  debug: true
+});
+app.use(sassMiddleware);
+
+
+
+// view engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
-swig.setDefaults({cache:false});
+app.engine('html', swig.renderFile);
 
-app.use(sassMiddleware({
-    src: __dirname + '/assets',
-    dest: path.join(__dirname, 'public'),
-    debug: true,
-    outputStyle: 'compressed',
-    prefix:  '/prefix'
-}));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./routes'))
 
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
-
-app.use('/', routes);
-
-
-// catch 404 (i.e., no route was hit) and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// handle all errors (anything passed into next())
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    console.log({error: err});
-    res.render('error'
-        // ... fill in this part
-    );
-});
-
-module.exports = app;
--- models/index.js
+++ models/index.js
@@ -1,46 +1,39 @@
 var mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost/tripplanner');
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
 var placeSchema = new mongoose.Schema({
-  address:  String,
-  city: 	String,
-  state: 	String,
-  phone:    String,
-  location: {type: [Number], length:2}// lat, lon
+  address: String,
+  city: String,
+  state: String,
+  phone: String,
+  location: [Number]
 });
 
 var hotelSchema = new mongoose.Schema({
-  name:  	 String, 
-  place: 	 String,
+  name: String,
+  place: [placeSchema],
   num_stars: {type: Number, min: 1, max: 5},
-  amenities: {type: String} //comma delimited string list
+  amenities: String
 });
 
 var thingToDoSchema = new mongoose.Schema({
-    name: String,
-    place: String,
-    age_range: String
-})
+  name: String,
+  place: [placeSchema],
+  age_range: String
+});
 
 var restaurantSchema = new mongoose.Schema({
-    name: String,
-    place: String,
-    cuisine: String,
-    price: {type:Number, min: 1, max: 5}
-})
-
-var Place = mongoose.model('Place', placeSchema);
-var Hotel = mongoose.model('Hotel', hotelSchema);
-var ThingToDo = mongoose.model('ThingToDo', thingToDoSchema);
-var Restaurant = mongoose.model('Restaurant', restaurantSchema)
+  name: String,
+  place: [placeSchema],
+  cuisine: String,
+  price: {type: Number, min: 1, max: 5}
+});
 
 module.exports = {
-  Place: Place,
-  Hotel: Hotel,
-  ThingToDo: ThingToDo,
-  Restaurant: Restaurant
+  Place:      mongoose.model('Place', placeSchema),
+  Hotel:      mongoose.model('Hotel', hotelSchema),
+  ThingToDo:  mongoose.model('ThingToDo', thingToDoSchema),
+  Restaurant: mongoose.model('Restaurant', restaurantSchema)
 };
